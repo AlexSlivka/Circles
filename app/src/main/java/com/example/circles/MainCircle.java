7ab89923c3d377d07294ea98752f31b7fca85308
Aleksei Slivka <alexprospero25@gmail.com>
2020-04-22 20:00:24 +0300

    Начало разработки игры "Разноцветные круги". Отрисовка базового круга на белом фоне.

---
+++ app/src/main/java/com/example/circles/MainCircle.java
@@ -0,0 +1,26 @@
+package com.example.circles;
+
+public class MainCircle {
+    public static final int INIT_ = 50;
+    private int x;
+    private int y;
+    private int radius;
+
+    public MainCircle(int x, int y) {
+        this.x = x;
+        this.y = y;
+        radius = INIT_;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+}
