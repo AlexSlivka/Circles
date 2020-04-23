552b38bb700e5df54cb63b9ac26d7bd87e41ec81
Aleksei Slivka <alexprospero25@gmail.com>
2020-04-23 22:00:49 +0300

    Добвленая коллекция вражеских кругов

--- app/src/main/java/com/example/circles/GameManager.java
+++ app/src/main/java/com/example/circles/GameManager.java
@@ -3,8 +3,12 @@ package com.example.circles;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
+import java.util.ArrayList;
+
 public class GameManager {
+    public static final int MAX_CIRCLES = 10;
     private MainCircle mainCircle;
+    private ArrayList<EnemyCircle> circles;
     private CanvasView canvasView;
     private static int width;
     private static int height;
@@ -15,7 +19,17 @@ public class GameManager {
         width = w;
         height = h;
         initMainCicle();
+        initEnemyCircles();
 
+    }
+
+    private void initEnemyCircles() {
+        circles = new ArrayList<EnemyCircle>();
+        for (int i = 0; i < MAX_CIRCLES; i++) {
+            EnemyCircle circle;
+            circle = EnemyCircle.getRandomCircle();
+            circles.add(circle);
+        }
     }
 
     public static int getWidth() {
@@ -33,6 +47,10 @@ public class GameManager {
 
     public void onDraw() {
         canvasView.drawCircle(mainCircle);
+        for (EnemyCircle circle: circles ) {
+            canvasView.drawCircle(circle);
+
+        }
     }
 
     public void onTouchEvent(int x, int y) {
