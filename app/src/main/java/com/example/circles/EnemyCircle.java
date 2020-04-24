d1d772b81129791a5fda0016661fed051e2a8711
Aleksei Slivka <alexprospero25@gmail.com>
2020-04-24 07:23:31 +0300

    Круги стали разных цветов кругов, красные вражеские

--- app/src/main/java/com/example/circles/EnemyCircle.java
+++ app/src/main/java/com/example/circles/EnemyCircle.java
@@ -1,5 +1,7 @@
 package com.example.circles;
 
+import android.graphics.Color;
+
 import java.util.Random;
 
 public class EnemyCircle extends SimpleCircle {
@@ -7,6 +9,7 @@ public class EnemyCircle extends SimpleCircle {
 
     public static final int FROM_RADIUS = 10;
     public static final int TO_RADIUS = 110;
+    public static final int ENEMY_COLOR = Color.RED;
 
     public EnemyCircle(int x, int y, int radius) {
         super(x, y, radius);
@@ -18,8 +21,8 @@ public class EnemyCircle extends SimpleCircle {
         int x = random.nextInt(GameManager.getWidth());
         int y = random.nextInt(GameManager.getHeight());
         int radius = FROM_RADIUS + random.nextInt(TO_RADIUS - FROM_RADIUS);
-        EnemyCircle enemyCircle = new EnemyCircle(x,y,radius);
-
+        EnemyCircle enemyCircle = new EnemyCircle(x, y, radius);
+        enemyCircle.setColor(ENEMY_COLOR);
         return enemyCircle;
 
     }
